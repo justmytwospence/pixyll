630e46580a5004bb429eaaae385a2453cc15787d
Spencer Boucher <spencer@spencerboucher.com>
2016-01-18 14:16:37 -0800

    Set up Reveal.js

---
+++ _plugins/reveal.rb
@@ -0,0 +1,20 @@
+module Jekyll
+  module Tags
+    class SlideTag < Liquid::Block
+      def initialize(tag_name, args, tokens)
+        super
+        @section_attributes = args
+      end
+
+      def render(context)
+        site = context.registers[:site]
+        converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+
+        content = converter.convert(super(context))
+        "<section #{@section_attributes}>#{content}</section>"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('slide', Jekyll::Tags::SlideTag)
